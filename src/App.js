3c4ac7a1229274145d814495968d5ee0cba529e2
ikhana <Inaa.eth@gmail.com>
2024-06-30 08:30:01 -0700

    How to buy

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 // App.js
 import React from 'react';
 import styled from 'styled-components';
-
+import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-
-
+import Banner from './components/Hero/HeroSection';
+import FartGridComponent from './components/FartGrid';
 import TokenEconomicsSections from './components/TokenEconomics';
 import MainBackground from './components/Background';
+import PartnersSection from './components/MovingStrip';
+import Roadmap from './components/Rodmap';
 
-
-
+import HallOfFame from './components/HallOfFame';
 import { GlobalStyle } from './GlobalStyle';
-
+import About from './components/About';
 import CTASection from './components/CTASection';
-
+import GameSection from './components/GameSection';
 
 const AppContainer = styled.div`
   position: relative;
@@ -27,11 +28,14 @@ function App() {
       <GlobalStyle/>
       <AppContainer>
        
-       
-      
-      
-        
-       
+        <NavBar />
+        <Banner />
+        <About/>
+        <GameSection/>
+        <HallOfFame/>
+        <FartGridComponent />
+        <PartnersSection/>
+        <Roadmap/>
         <TokenEconomicsSections />
         <CTASection/>
        
